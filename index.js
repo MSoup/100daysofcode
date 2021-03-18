6bbcb2b2f96299b5fed1cb4cb3add8a4736be59f
Daven <19704779+MSoup@users.noreply.github.com>
2021-03-18 12:54:01 +0900

    Create index.js

--- index.js
+++ index.js
@@ -1,32 +1,75 @@
 // Add test dots
 
 const dotContainer = document.querySelector(".imgIndicator");
+const display = document.querySelector(".displayWrapper");
 
+// holds DOM elements
 const imgList = [];
 
-// Test function
-
+// just for testing, loads imgList with test pictures
 function generatePictures() {
   const images = [
-    { id: 1, src: "./assets/image01.jpg", title: "foo", description: "bar" },
-    { id: 2, src: "./assets/image02.jpg", title: "foo", description: "bar" },
-    { id: 3, src: "./assets/image03.jpg", title: "foo", description: "bar" },
-    { id: 4, src: "./assets/image04.jpg", title: "foo", description: "bar" },
-    { id: 5, src: "./assets/image05.jpg", title: "foo", description: "bar" },
+    {
+     id: 0, 
+     src: "https://css-tricks.com/wp-content/uploads/2013/11/a-blue-box.png", 
+     title:"test image", 
+     description: "this is a description",
+    },
+        {
+     id: 1, 
+     src: "https://jooinn.com/images/red-box-1.jpg", 
+     title:"test image", 
+     description: "this is a description",
+    },
   ];
-  // do stuff
-}
+  
+  let domImg;
+  images.forEach((image) => 
+                {
+    domImg = document.createElement("img");
+    domImg.setAttribute("id", image.id);
+    domImg.setAttribute("src", image.src);
+    domImg.setAttribute("title", image.title);       
+    domImg.setAttribute("description", image.description);
+    imgList.push(domImg);
+    display.appendChild(domImg);
+  });
+} 
 
+// generates a dot for each picture
 function generateDots() {
   let newImg;
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < imgList.length; i++) {
     newImg = document.createElement("button");
 
     newImg.classList.add("dot");
+    newImg.classList.add("index-" + i);
 
     dotContainer.appendChild(newImg);
   }
 }
 
 // call the tests
+generatePictures()
 generateDots();
+
+console.log(imgList);
+
+var curPicIndex = -1;
+
+function advancePicture() {
+    ++curPicIndex;
+    if (curPicIndex >= imgList.length) {
+        curPicIndex = 0;
+    }
+    setPicture(imgList[curPicIndex]); 
+}
+
+function setPicture(pic) {
+    imgList.forEach((image) => image.classList.remove("set"));
+    pic.classList.add("set");
+}
+
+var intervalID = setInterval(advancePicture, 5000);
+
+
